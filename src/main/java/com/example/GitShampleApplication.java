3e2f87841a9c82f39de64863a29dfeb3b29c9c3b
wasshiiiiiii <washio.imp@gmail.com>
2022-05-10 15:41:33 +0900

    first

---
+++ src/main/java/com/example/GitShampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitShampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitShampleApplication.class, args);
+	}
+
+}
